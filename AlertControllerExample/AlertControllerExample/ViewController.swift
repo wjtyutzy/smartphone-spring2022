f5d8a68924224e448966ca0353771a11a7b60419
wjtyutzy <wang.jing16@northeastern.edu>
2022-02-27 11:29:35 -0800

    checking in code for Alert Controller

---
+++ AlertControllerExample/AlertControllerExample/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  AlertControllerExample
+//
+//  Created by laputer on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var lblAlert: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func pressMe(_ sender: Any) {
+        
+        var txtField: UITextField?
+        
+        let alertController = UIAlertController(title: "Sample Alert Controller", message: "Simple Message", preferredStyle: .alert)
+        
+        let OKButton = UIAlertAction(title: "OK", style: .default) { action in
+            print("OK Button Pressed")
+            self.lblAlert.text = txtField?.text
+        }
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("Cancel Button Pressed")
+        }
+        
+        alertController.addAction(OKButton)
+        alertController.addAction(cancelButton)
+        
+        alertController.addTextField { lblTextField in
+            lblTextField.placeholder = "Type Something"
+            txtField = lblTextField
+            //let text = self.lblAlert.text
+            //self.lblAlert.text = lblTextField.text
+            
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+        
+    }
+}
+
