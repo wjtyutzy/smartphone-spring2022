5bbf6a650b6fbb1d1b1bfb5df02ee4c73409cf69
wjtyutzy <wang.jing16@northeastern.edu>
2022-02-27 11:01:27 -0800

    checking in code to delete

--- TwoTables/TwoTables/ViewController.swift
+++ TwoTables/TwoTables/ViewController.swift
@@ -46,8 +46,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             cell.textLabel?.text = arr2[indexPath.row]
             return cell
         }
-        
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            if tableView == tbl1 {
+                arr1.remove(at: indexPath.row)
+            }else {
+                arr2.remove(at: indexPath.row)
+            }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
 
 }
 
